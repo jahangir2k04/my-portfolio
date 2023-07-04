89c585de42f4d65a7ee46b16b8fd6f63fb650655
jahangir2k04 <jahangir2k04@gmail.com>
2023-07-04 20:43:36 +0600

    navbar added

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import Footer from "./components/Footer"
+import Navbar from "./components/Navbar"
+
+
+
+function App() {
+
+  return (
+    <div>
+      <Navbar></Navbar>
+      <Footer></Footer>
+    </div>
+  )
+}
+
+export default App
